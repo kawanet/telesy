4c1372d9d823f616372fd1d24ae65b645874330c
Yusuke Kawasaki <u-suke@kawa.net>
2022-10-29 13:52:06 +0900

    ECMAScript reserved words
    https://tc39.es/ecma262/#sec-keywords-and-reserved-words

--- mustache/mustache2telesy.cli.ts
+++ mustache/mustache2telesy.cli.ts
@@ -8,6 +8,15 @@ CLI(process.stdout);
 function CLI(stream: { write(str: string): any }) {
     stream.write(`import {$$, $$$} from "telesy";\n\n`);
 
+    // @see https://tc39.es/ecma262/#sec-keywords-and-reserved-words
+    const reservedWords = `await break case catch class const continue debugger default delete do
+    else enum export extends false finally for function if import in instanceof new null return
+    super switch this throw true try typeof var void while with yield
+    enum implements interface package private protected public arguments eval`;
+
+    const check: { [name: string]: number } = {};
+    reservedWords.split(/\W+/).forEach(v => check[v] = 0);
+
     const files = process.argv.slice(2);
 
     if (!files.length) {
@@ -19,7 +28,16 @@ function CLI(stream: { write(str: string): any }) {
         let source = fs.readFileSync(file, "utf-8");
         source = source.replace(/^\s+/mg, "").replace(/\s+\n/g, "\n");
 
-        const name = file.split("/").pop()?.split(".").shift()?.replace(/\W/g, "_");
+        let name = file.split("/").pop()?.split(".").shift()?.replace(/\W/g, "_")!;
+        if (check[name] == null) {
+            check[name] = 1;
+        } else {
+            const prefix = name;
+            while (check[name = `${prefix}_${++check[name]}`]) {
+                //
+            }
+            check[name] = 0;
+        }
 
         const code = mustache2telesy(source);
 
