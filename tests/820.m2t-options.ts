b9354e27a073ecb515bf5e3f92d206b3a23b982e
Yusuke Kawasaki <u-suke@kawa.net>
2022-10-30 10:14:21 +0900

    refactor: optionRender

--- tests/820.m2t-options.ts
+++ tests/820.m2t-options.ts
@@ -8,8 +8,6 @@ const TITLE = __filename.split("/").pop()!;
 
 const compile = (source: string) => Function("$$", "$$$", "return " + source)($$, $$$);
 
-const removeSpaces = (s: string) => s.replace(/(>)\s+|\s+(<)/g, "$1$2");
-
 describe(TITLE, () => {
     it(`{trim: true}`, () => {
         const html = `
@@ -17,14 +15,23 @@ describe(TITLE, () => {
         <span>Hi!</span>
         {{/ bool }}
         `;
-        const normalRender = compile(mustache2telesy(html));
-        const betterRender = compile(mustache2telesy(html, {trim: true}));
-        assert.notEqual(String(betterRender), String(normalRender));
 
-        const normalResult = normalRender({bool: true});
-        const trimedResult = betterRender({bool: true});
-        assert.notEqual(trimedResult, normalResult);
-        assert.equal(removeSpaces(trimedResult), removeSpaces(normalResult));
+        const normalRender = compile(mustache2telesy(html));
+        const optionRender = compile(mustache2telesy(html, {trim: true}));
+        assert.match(String(normalRender), /\$\$`\s+\$\{/);
+        assert.match(String(optionRender), /\$\$`\$\{/);
+
+        {
+            const data = {};
+            assert.match(normalRender(data), /^\s+$/);
+            assert.match(optionRender(data), /^$/);
+        }
+
+        {
+            const data = {bool: true};
+            assert.match(normalRender(data), /^\s+<span>Hi!<\/span>\s+$/);
+            assert.equal(optionRender(data), `<span>Hi!</span>`);
+        }
     });
 
     it(`{guess: true} (Array)`, () => {
@@ -37,18 +44,20 @@ describe(TITLE, () => {
         </ul>
         {{/ array.length }}
         `;
+
         const normalRender = compile(mustache2telesy(html, {guess: false, trim: true}));
-        const betterRender = compile(mustache2telesy(html, {guess: true, trim: true}));
-        assert.notEqual(String(betterRender), String(normalRender));
+        const optionRender = compile(mustache2telesy(html, {guess: true, trim: true}));
+        assert.match(String(normalRender), /Array.isArray/);
+        assert.doesNotMatch(String(optionRender), /Array.isArray/);
 
         {
             const data = {};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), optionRender(data));
         }
 
         {
             const data = {array: [1, 2, 3]};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), optionRender(data));
         }
     });
 
@@ -59,126 +68,126 @@ describe(TITLE, () => {
         <span>{{ qux }}</span>
         {{/ foo.bar }}
         `;
+
         const normalRender = compile(mustache2telesy(html, {trim: true, guess: false}));
-        const betterRender = compile(mustache2telesy(html, {trim: true, guess: true}));
-        // console.warn(String(betterRender));
-        // console.warn(String(normalRender));
-        assert.notEqual(String(betterRender), String(normalRender));
+        const optionRender = compile(mustache2telesy(html, {trim: true, guess: true}));
+        assert.match(String(normalRender), /Array.isArray/);
+        assert.doesNotMatch(String(optionRender), /Array.isArray/);
 
         {
             const data = {};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), optionRender(data));
         }
 
         {
             const data = {foo: {bar: {buz: "BUZ", qux: "QUX"}}};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), optionRender(data));
         }
     });
 
     it(`{guess: true} (Function)`, () => {
         const html = `<span>{{ foo.toString }}</span>`;
         const normalRender = compile(mustache2telesy(html, {trim: true, guess: false}));
-        const betterRender = compile(mustache2telesy(html, {trim: true, guess: true}));
+        const optionRender = compile(mustache2telesy(html, {trim: true, guess: true}));
 
-        assert.notEqual(String(betterRender), String(normalRender));
         assert.ok(!/\.toString\(\)/.test(normalRender), `function not detected without {guess: true}`);
-        assert.ok(/\.toString\(\)/.test(betterRender), `function detected with {guess: true}`);
+        assert.ok(/\.toString\(\)/.test(optionRender), `function detected with {guess: true}`);
 
         {
             const data = {};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), `<span></span>`);
+            assert.equal(optionRender(data), `<span></span>`);
         }
 
         {
             const data = {foo: {toString: () => "FOO"}};
-            assert.equal(betterRender(data), `<span>FOO</span>`);
+            assert.equal(optionRender(data), `<span>FOO</span>`);
         }
     });
 
     it(`{boolean: "isFoo"}`, () => {
-        const html = `
-        {{# foo.isFoo }}
-        <span>Foo</span>
-        {{/ foo.isFoo }}
-        `;
+        const html = `<span>{{# foo.isFoo }}Foo{{/ foo.isFoo }}</span>`;
         const normalRender = compile(mustache2telesy(html, {trim: true}));
-        const betterRender = compile(mustache2telesy(html, {trim: true, boolean: "isFoo"}));
-        assert.notEqual(String(betterRender), String(normalRender));
+        const optionRender = compile(mustache2telesy(html, {trim: true, boolean: "isFoo"}));
+        assert.match(String(normalRender), /Array.isArray/);
+        assert.doesNotMatch(String(optionRender), /Array.isArray/);
 
         {
             const data = {};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), `<span></span>`);
+            assert.equal(optionRender(data), `<span></span>`);
         }
 
         {
             const data = {foo: {isFoo: true}};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), `<span>Foo</span>`);
+            assert.equal(optionRender(data), `<span>Foo</span>`);
         }
     });
 
     it(`{array: "fooList"}`, () => {
-        const html = `
-        {{# fooList }}
-        <li>{{.}}</li>
-        {{/ fooList }}
-        `;
+        const html = `<ul>{{# fooList }}<li>{{.}}</li>{{/ fooList }}</ul>`;
         const normalRender = compile(mustache2telesy(html, {trim: true}));
-        const betterRender = compile(mustache2telesy(html, {trim: true, array: "fooList"}));
-        assert.notEqual(String(betterRender), String(normalRender));
+        const optionRender = compile(mustache2telesy(html, {trim: true, array: "fooList"}));
+        assert.match(String(normalRender), /Array.isArray/);
+        assert.doesNotMatch(String(optionRender), /Array.isArray/);
 
         {
             const data = {};
-            assert.equal(normalRender(data), betterRender(data));
+            assert.equal(normalRender(data), `<ul></ul>`);
+            assert.equal(optionRender(data), `<ul></ul>`);
         }
 
         {
             const data = {fooList: [1, 2, 3]};
-            assert.equal(normalRender(data), betterRender(data));
+            const expected = `<ul><li>1</li><li>2</li><li>3</li></ul>`;
+            assert.equal(normalRender(data), expected);
+            assert.equal(optionRender(data), expected);
         }
     });
 
     it(`{object: "bar"}`, () => {
         const html = `<span>{{# foo.bar }}{{buz}}{{/ foo.bar }}</span>`;
         const normalRender = compile(mustache2telesy(html, {trim: true}));
-        const betterRender = compile(mustache2telesy(html, {trim: true, object: "bar"}));
-        assert.notEqual(String(betterRender), String(normalRender));
+        const optionRender = compile(mustache2telesy(html, {trim: true, object: "bar"}));
+        assert.match(String(normalRender), /Array.isArray/);
+        assert.doesNotMatch(String(optionRender), /Array.isArray/);
 
         {
             const data = {};
             assert.equal(normalRender(data), `<span></span>`);
-            assert.equal(betterRender(data), `<span></span>`);
+            assert.equal(optionRender(data), `<span></span>`);
         }
 
         {
             const data = {foo: {bar: true}, buz: "YYY"};
             assert.equal(normalRender(data), `<span>YYY</span>`);
-            assert.equal(betterRender(data), `<span></span>`);
+            assert.equal(optionRender(data), `<span></span>`);
         }
 
         {
             const data = {foo: {bar: {buz: "XXX"}}, buz: "YYY"};
             assert.equal(normalRender(data), `<span>XXX</span>`);
-            assert.equal(betterRender(data), `<span>XXX</span>`);
+            assert.equal(optionRender(data), `<span>XXX</span>`);
         }
     });
 
     it(`{func: "getText"}`, () => {
         const html = `<span>{{ foo.getText }}</span>`;
         const normalRender = compile(mustache2telesy(html, {trim: true}));
-        const betterRender = compile(mustache2telesy(html, {trim: true, func: "getText"}));
+        const optionRender = compile(mustache2telesy(html, {trim: true, func: "getText"}));
         assert.doesNotMatch(String(normalRender), /\.getText\(\)/);
-        assert.match(String(betterRender), /\.getText\(\)/);
+        assert.match(String(optionRender), /\.getText\(\)/);
 
         {
             const data = {};
             assert.equal(normalRender(data), `<span></span>`);
-            assert.equal(betterRender(data), `<span></span>`);
+            assert.equal(optionRender(data), `<span></span>`);
         }
         {
             const data = {foo: {getText: () => "FOO"}};
             assert.notEqual(normalRender(data), `<span>FOO</span>`);
-            assert.equal(betterRender(data), `<span>FOO</span>`);
+            assert.equal(optionRender(data), `<span>FOO</span>`);
         }
     });
 });
