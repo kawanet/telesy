f9db6ee050b6d18eac192eaf7ec1c7764ecc5201
Yusuke Kawasaki <u-suke@kawa.net>
2022-11-17 11:28:47 +0900

    refactor: TemplateArgments

--- src/telesy.ts
+++ src/telesy.ts
@@ -4,6 +4,8 @@
 
 import type {Telesy} from "../";
 
+type Stringify = (v: V) => string;
+type TemplateArgments = [TemplateStringsArray, ...V[]];
 type EscapeFn = (str: string) => string;
 
 const AMP = {"<": "&lt;", "&": "&amp;", ">": "&gt;", '"': "&quot;", "'": "&apos;"};
@@ -12,26 +14,28 @@ const escapeHTML: EscapeFn = v => v.replace(/([<&>"'])/g, ($1: keyof typeof AMP)
 
 const isTemplateStringsArray = (v: any): v is TemplateStringsArray => (v && v.raw && (v.raw.length > 0));
 
-const isFragment = (v: any): v is Telesy.Fragment => ("string" === typeof (v as Telesy.Fragment).outerHTML);
+const isFragment = (v: any): v is Fragment => ("string" === typeof v.outerHTML);
+
+const toString = function (this: Fragment) {
+    return this.outerHTML;
+};
 
 const makeTelesy = (escapeFn: EscapeFn): Telesy.telesy => {
-
-    type Stringify = (v: Telesy.V) => string;
 
     // stringify function with escaping feature
     const stringify$$: Stringify = v => {
         if ("string" === typeof v) {
             return escapeFn(v);
+        } else if ("number" === typeof v) {
+            return escapeFn(String(v));
         } else if (v == null || v === false) {
             return "";
-        }
-
-        if (isFragment(v)) {
+        } else if (isFragment(v)) {
             return v.outerHTML;
         } else if (Array.isArray(v)) {
             return v.map(stringify$$).join(""); // recursive call
         } else {
-            return escapeFn(String(v));
+            return escapeFn(String(v)); // default behaviour
         }
     };
 
@@ -39,21 +43,21 @@ const makeTelesy = (escapeFn: EscapeFn): Telesy.telesy => {
     const stringify$$$: Stringify = v => {
         if ("string" === typeof v) {
             return v;
+        } else if ("number" === typeof v) {
+            return String(v);
         } else if (v == null || v === false) {
             return "";
-        }
-
-        if (isFragment(v)) {
+        } else if (isFragment(v)) {
             return v.outerHTML;
         } else if (Array.isArray(v)) {
             return v.map(stringify$$$).join(""); // recursive call
         } else {
-            return String(v);
+            return String(v); // default behaviour
         }
     };
 
     // template literals
-    const tag$$ = (t: TemplateStringsArray, args: [any, ...string[]]): string => {
+    const tag$$ = (t: TemplateStringsArray, args: TemplateArgments): string => {
         const size = t.length;
         if (size === 1) {
             return t[0];
@@ -64,40 +68,36 @@ const makeTelesy = (escapeFn: EscapeFn): Telesy.telesy => {
         } else {
             let str = t[0];
             for (let i = 1; i < size; i++) {
-                str += stringify$$(args[i]);
+                str += stringify$$(args[i] as V);
                 str += t[i];
             }
             return str;
         }
     };
 
-    const $$: Telesy.$$ = function (t) {
+    const $$ = function (t: TemplateStringsArray | string) {
         if (isTemplateStringsArray(t)) {
             // Template Literals: $$`<div>${v}</div>`
-            return tag$$(t, arguments as any);
+            return tag$$(t, arguments as any as TemplateArgments);
         } else {
             // Function Call: $$($$$("<li>${v}</li>"))
             return stringify$$(t);
         }
-    };
+    } as Telesy.$$;
 
-    const $$$: Telesy.$$$ = function (t) {
+    const $$$ = function (t: TemplateStringsArray | string) {
         if (isTemplateStringsArray(t)) {
-            // Template Literals: $$`<div>${v}</div>`
-            t = tag$$(t, arguments as any);
+            // Template Literals: $$$`<div>${v}</div>`
+            t = tag$$(t, arguments as any as TemplateArgments);
         } else {
             // Function Call: $$($$$("<li>${v}</li>"))
             t = stringify$$$(t);
         }
 
         return {outerHTML: t, toString};
-    };
+    } as Telesy.$$$;
 
     return {$$, $$$};
-}
-
-function toString(this: Telesy.Fragment) {
-    return this.outerHTML;
 }
 
 export const {$$, $$$} = makeTelesy(escapeHTML);
